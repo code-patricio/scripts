df04f8beb310ca2454910c5fa55ae136e4f006d4
code-patricio <pat.gb.moreno@gmail.com>
2023-05-22 12:22:08 -0300

    c

---
+++ c/idk.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+
+int main(int argc, char** argv) {
+
+    int input;
+    scanf("%d", &input);
+    printf("Hola, %d\n¿Que edad tienes?", input);
+//    char** nombre;
+ //   scanf("%s", &nombre);
+  //  printf("\nNo tienes %s", nombre);
+    return 0;
+}
