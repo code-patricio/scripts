c49e836ecdef6e416939babcdcac07ed60c2f5c2
code-patricio <pat.gb.moreno@gmail.com>
2023-06-01 20:44:38 -0300

    idk

--- c/k_r/ch1_fhr_cls_table.c
+++ c/k_r/ch1_fhr_cls_table.c
@@ -12,10 +12,8 @@ step = 20;
 fahr = lower;
 while(fahr <= upper){
     celsius = 5 * (fahr-32) / 9;
-    printf("%d\t%d\n", fahr, celsius);
+    printf("%3d %6d\n", fahr, celsius);
     fahr = fahr + step;
     }
-
-return 0;
 
 }
