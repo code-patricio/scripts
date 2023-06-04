9cb1966d1215d8f85ab0805be684cc6c3e614cf7
Casa <casa@pop-os.localdomain>
2023-06-03 23:25:40 -0300

    rm compiled files

--- c/k_r/ch1/file_copying_x1.c
+++ c/k_r/ch1/file_copying_x1.c
@@ -1,8 +1,9 @@
 #include <stdio.h>
 
 int main() {
+    int bolean = getchar() != EOF;
+    printf("%d", bolean);
     int c;
     while ((c = getchar()) != EOF)
         putchar(c);
-    printf("%d", getchar() != EOF);
 }
