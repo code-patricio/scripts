f156a9982c0216753136ad51f1325bb938d19d5e
code-patricio <pat.gb.moreno@gmail.com>
2024-04-06 22:51:58 -0300

    update

--- c/k_r/ch1/1-6_file_copying.c
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
-
-main() {
-    printf("Escribe algo:\n");
-    
-    int bolean = getchar() != -1;
-    printf("bolean es igual a: %d\n", bolean);
-}
--- c/k_r/ch1/1-7_file_copying.c
+++
@@ -1,7 +0,0 @@
-#include <stdio.h>
-
-int main() {
-    int bolean = EOF;
-    printf("%d\n", bolean);
-}
-
