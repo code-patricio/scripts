f2a86411d6265e598cae9f8b5c8c08d2b2e09c71
code-patricio <pat.gb.moreno@gmail.com>
2024-09-25 22:20:44 +0000

    java

---
+++ java/cs-princeton-java/ch1/ch1.1/UseArgument.java
@@ -0,0 +1,7 @@
+public class UseArgument {
+    public static void main(String[] args) {
+        System.out.print("Hola, ");
+        System.out.print(args[0]);
+        System.out.println(". ¿Como estas?");
+    }
+}
---
+++ java/cs-princeton-java/ch1/ch1.1/UseThree.java
@@ -0,0 +1,12 @@
+public class UseThree {
+
+    public static void main(String[] args) {
+        System.out.print("Hola ");
+        System.out.print(args[0]);
+        System.out.print(", ");
+        System.out.print(args[1]);
+        System.out.print(" y ");
+        System.out.print(args[2]);
+        System.out.println(". ¿Como estan?");
+    }
+}
